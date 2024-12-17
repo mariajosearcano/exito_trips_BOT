f891c06121e71c6ff27c2207f80c3efe1780cce7
Embm5 <emmanuel_bolivar82212@elpoli.edu.co>
2024-12-17 12:22:16 -0500

    Crate bot
    added funtions on the bot

--- index.py
+++ index.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+chrome_options = Options()
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])  # Evita logs innecesarios
+chrome_options.add_argument("--disable-usb")  # Desactiva búsqueda USB
+chrome_options.add_argument("--ignore-certificate-errors")  # Ignora errores SSL
+chrome_options.add_argument("--disable-extensions")  # Desactiva extensiones
+
 # Connect with the driver and open the browser
 service = Service("driver/chromedriver.exe")
-bot = webdriver.Chrome(service=service)
+bot = webdriver.Chrome(service=service, options=chrome_options)
 bot.maximize_window()
 # Open the page
 bot.get("https://www.viajesexito.com")
@@ -73,7 +80,7 @@ time.sleep(1)
 # Select the return date
 returnDate = bot.find_element(By.XPATH, '/html/body/div[9]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[3]/div[1]')
 time.sleep(1)
-returnDate.click()
+returnDate.click()                      
 time.sleep(1)
 # Click on the button to accept
 accept = bot.find_element(By.XPATH, '/html/body/div[9]/div[2]/div[2]/div[2]/button[2]')
@@ -107,53 +114,52 @@ time.sleep(1)
 search = bot.find_element(By.XPATH, '//*[@id="sbm_netactica_airhotel"]')
 time.sleep(1)
 search.click()
-time.sleep(50)
+time.sleep(10)
 
 # Go to new tab
 bot.switch_to.window(bot.window_handles[1])
 
 
 # Show packages
+packages = WebDriverWait(bot, 10).until(
+    ec.presence_of_all_elements_located(
+        (By.XPATH, '//*[@id="divAirResults"]/div/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
+    )
+)
+
+
 print("Price of all packages")
 print("")
-
-for i in range(0, 9):
-    package = WebDriverWait(bot, 10).until(
-        ec.presence_of_element_located((By.XPATH, '//*[@id="divAirResults"]/div[' + str(i+1) + ']/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')))
-    print("Package " + str(i+1) + ": " + package.text)
+for idx, package in enumerate(packages):
+    print(f"Package {idx + 1}: {package.text}")
 
 print("")
+# print("Price of all packages")
+# print("")
 
-# # Select the first package
-# # firstPackage = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[6]/div[1]/div/div/div[2]/div/div[1]/div[3]/p[1]/span[2]')
-# firstPackage = bot.find_element(By.XPATH, '//*[@id="divAirResults"]/div[1]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# print("First package: " + firstPackage.text)
-# time.sleep(3)
-#
-# # Select the second package
-# # secondPackage = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[6]/div[2]/div/div/div[2]/div/div[1]/div[3]/p[1]/span[2]')
-# secondPackage = bot.find_element(By.XPATH, '//*[@id="divAirResults"]/div[2]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# print("Second package: " + secondPackage.text)
-# time.sleep(3)
-#
-# # Select the third package
-# # thirdPackage = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[6]/div[3]/div/div/div[2]/div/div[1]/div[3]/p[1]/span[2]')
-# thirdPackage = bot.find_element(By.XPATH, '//*[@id="divAirResults"]/div[3]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# print("Third package: " + thirdPackage.text)
-# time.sleep(3)
-#
-# # Select the fourth package
-# # fourthPackage = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[6]/div[4]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# fourthPackage = bot.find_element(By.XPATH, '//*[@id="divAirResults"]/div[4]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# print("Fourth package: " + fourthPackage.text)
-# time.sleep(3)
-#
-# # Select the fifth package
-# # fifthPackage = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[6]/div[5]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# fifthPackage = bot.find_element(By.XPATH, '//*[@id="divAirResults"]/div[5]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
-# print("Fifth package: " + fifthPackage.text)
-# time.sleep(3)
-#
+# for i in range(0, 9):
+#     package = WebDriverWait(bot, 10).until(
+#         ec.presence_of_element_located((By.XPATH, '//*[@id="divAirResults"]/div[' + str(i+1) + ']/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')))
+#     print("Package " + str(i+1) + ": " + package.text)
+
+# print("")
+
+
+#show airline
+airline = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[5]/div/div/div[4]/div[1]/input')
+time.sleep(30)
+airline.click()
+time.sleep(20)
+
+
+#whasapp footer
+whatsapp = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[1]/header/div[1]/div[2]/nav/div/div[1]/a')
+time.sleep(5)
+whatsapp.click()
+time.sleep(10)
+
+
+
 # # Select the sixth package
 # # sixthPackage = bot.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[2]/div[4]/div/div/div/div[2]/div[7]/div/div[1]/div[6]/div[6]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
 # sixthPackage = bot.find_element(By.XPATH, '//*[@id="divAirResults"]/div[6]/div/div/div[2]/div/div[1]/div[2]/p[1]/span[2]')
@@ -190,32 +196,5 @@ print("")
 # time.sleep(3)
 
 
-# Do some scroll
-bot.execute_script("window.scrollTo(0, 500)")
-time.sleep(1)
-
-# Advanced options
-advancedOptions = bot.find_element(By.XPATH, '//*[@id="aShowHideAdvanced"]')
-time.sleep(1)
-advancedOptions.click()
-time.sleep(1)
-# Select Airline
-inputAirline = "Avianca"
-airline = bot.find_element(By.XPATH, '//*[@id="txtAirlineCode"]')
-time.sleep(1)
-airline.click()
-time.sleep(1)
-airline.send_keys(inputAirline)
-time.sleep(1)
-# Select option
-optionAirline = bot.find_element(By.XPATH, '//*[@id="ui-id-4"]/li[1]/a')
-time.sleep(1)
-optionAirline.click()
-time.sleep(1)
-# Apply the changes
-applyChanges = bot.find_element(By.XPATH, '//*[@id="ulNewSearch"]/div[8]/input')
-time.sleep(1)
-applyChanges.click()
-time.sleep(30)
 
 
